933f05b9a66698c2188f8b91f0af34ed4b7f201a
hhw12409 <sksk2425@naver.com>
2022-10-15 01:42:25 +0900

    Feat: make ErrorMessage component

---
+++ src/components/ErrorMessage/ErrorMessage.tsx
@@ -0,0 +1,11 @@
+const ErrorMessage = () => {
+  return (
+    <div>
+      <h2>ERROR!</h2>
+      <h2>sorry man</h2>
+      <h2>you sure you got the right pokemon id?</h2>
+    </div>
+  )
+}
+
+export default ErrorMessage
