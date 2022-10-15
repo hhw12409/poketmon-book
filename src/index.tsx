4077ef28b52eb94b022c47fbeddafd8e0b7f97ae
hhw12409 <sksk2425@naver.com>
2022-10-15 23:06:22 +0900

    Feat: setting recoil

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RecoilRoot } from 'recoil'
 import App from './App'
 import Globalstyle from './Globalstyle'
 
@@ -9,9 +10,11 @@ const queryClient = new QueryClient()
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
-    <QueryClientProvider client={queryClient}>
-      <Globalstyle />
-      <App />
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Globalstyle />
+        <App />
+      </QueryClientProvider>
+    </RecoilRoot>
   </React.StrictMode>,
 )
