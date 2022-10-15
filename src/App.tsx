7bea9eed4b130e924905e934154424209d271569
hhw12409 <sksk2425@naver.com>
2022-10-15 23:03:34 +0900

    Docs: delete my poke page

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
-import MyPokemon from './pages/MyPokemon'
 import NotFound from './pages/NotFound'
 
 function App() {
@@ -8,7 +7,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/myPokemon" element={<MyPokemon />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,12 +6,20 @@ import Header from '../components/Header/Header'
 import GetForm from '../components/GetForm/GetForm'
 import PokeInfo from '../components/PokeInfo/PokeInfo'
 import Loading from '../components/Loading/Loading'
+import { useRecoilState } from 'recoil'
+import isClick from '../atoms/isClick'
 
 const Home = () => {
   const [pokeId, setPokeId] = useState('')
-
+  const [isBtnClick, setIsBtnClick] = useRecoilState(isClick)
   const { isLoading, data } = useQuery(['pokemon', pokeId], API.getPokeMon, {
-    enabled: !!pokeId,
+    enabled: isBtnClick,
+    onSuccess: () => {
+      setIsBtnClick(false)
+    },
+    onError: e => {
+      console.log(e)
+    },
   })
 
   const onChangePokeIdValue = (event: React.FormEvent<HTMLInputElement>) => {
@@ -25,7 +33,7 @@ const Home = () => {
     <Container>
       <Header />
       <GetForm pokeId={pokeId} onChange={onChangePokeIdValue} />
-      {isLoading && pokeId ? (
+      {isLoading && isBtnClick ? (
         <Loading />
       ) : (
         <PokeInfo data={data} pokeId={pokeId} />
